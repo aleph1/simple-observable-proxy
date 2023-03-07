bbe1bddc64de8e5b20ed094e6f86b89437230478
Daniel Barber <d.barber@aleph-1.com>
2023-03-07 01:48:14 -0500

    Major internal factor removes Observable class and need for new

--- index.js
+++ index.js
@@ -1,92 +1,85 @@
 // data that is being observed
-const observables = new WeakSet();
-// Observable instanced stored by their .proxy
-const observablesByProxy = new WeakMap();
-// which observables will be notified on tick
-const notify = new Set();
+const observables = new Set();
+// original data for each proxy
+const observablesByProxy = new Map();
+// observers for each proxy
+const observersByProxy = new Map();
+// observers to call on tick
+const proxiesToNotify = new Set();
 
 const tick = () => {
-  notify.forEach(observable => {
-    observable.observers.forEach(callback => callback());
+  proxiesToNotify.forEach(proxy => {
+    observersByProxy.get(proxy).forEach(callback => callback());
   });
-  notify.clear();
+  proxiesToNotify.clear();
   window.requestAnimationFrame(tick);
 };
 
-class Observable {
-
-  constructor(data, root) {
-    if(observables.has(data)) throw new Error('Can’t observe Observable.proxy');
-    const self = this;
-    const proxy = new Proxy(data, {
-      get(target, key) {
-        return target[key];
-      },
-      set(target, key, value) {
-        if(target[key] !== value) {
-          // self.proxy is assigned at the end of the Observable constructor,
-          // so it is safe to use it to see if observers need to be notified
-          if(self.proxy) {
-            if(observablesByProxy.has(value)) throw new Error('Can’t nest observables');
-            notify.add(root || self)
-          }
-          target[key] = value;
+const makeObservableProxy = (data, rootProxy) => {
+  if(observables.has(data)) throw new Error('Proxy of data already exists');
+  let notify = false;
+  const observers = new Set();
+  const proxy = new Proxy(data, {
+    get(target, key) {
+      return target[key];
+    },
+    set(target, key, value) {
+      if(target[key] !== value) {
+        // self.proxy is assigned at the end of the Observable constructor,
+        if(notify) {
+          if(observablesByProxy.has(value)) throw new Error('Can’t nest observables');
+          proxiesToNotify.add(rootProxy || proxy)
         }
-        return true;
-      },
-      deleteProperty(target, key) {
-        if (key in target) {
-          delete target[key];
-          notify.add(root || self);
-          return true;
-        }
-        return false;
+        target[key] = value;
       }
-    });
-    // Object.keys on array only includes keys that are specifically set
-    (Array.isArray(data) ? [...Array(data.length).keys()] : Object.keys(data)).forEach(key => {
-      const value = data[key];
-      if(observablesByProxy.has(value)) throw new Error('Can’t nest observables');
-      if(Array.isArray(value) || (typeof value === 'object' && value instanceof Object)) proxy[key] = new Observable(value, self).proxy;
-    });
-    self.observers = new Set();
-    observablesByProxy.set(self.proxy = proxy, self);
-    observables.add(self.data = data);
-  }
+      return true;
+    },
+    deleteProperty(target, key) {
+      if (key in target) {
+        delete target[key];
+        proxiesToNotify.add(rootProxy || proxy);
+        return true;
+      }
+      return false;
+    }
+  });
+  (Array.isArray(data) ? [...Array(data.length).keys()] : Object.keys(data)).forEach(key => {
+    const value = data[key];
+    if(observablesByProxy.has(value)) throw new Error('Can’t nest observables');
+    if(Array.isArray(value) || (typeof value === 'object' && value instanceof Object)) proxy[key] = makeObservableProxy(value, rootProxy || proxy);
+  });
+  notify = true;
+  observables.add(data);
+  observablesByProxy.set(proxy, data);
+  observersByProxy.set(proxy, new Set());
+  return proxy;
+};
 
-  observe(callback) {
-    return this.observers.add(callback);
-  }
-
-  unobserve(callback) {
-    return this.observers.delete(callback);
-  }
-
-  destroy() {
-    // Set.prototype.clear() returns undefined
-    this.observers = this.observers.clear();
-    return observables.delete(this.data) && observablesByProxy.delete(this.proxy);
-  }
-  
+export const observable = (data) => {
+  return (Array.isArray(data) || (typeof data === 'object' && data instanceof Object)) && makeObservableProxy(data);
 }
 
-export function observable(data) {
-  return (Array.isArray(data) || (typeof data === 'object' && data instanceof Object)) && new Observable(data).proxy;
+export const observe = (observableProxy, callback) => {
+  const observers = observersByProxy.get(observableProxy);
+  return observers && typeof callback === 'function' ? observers.add(callback) : false;
 }
 
-export function observe(observableProxy, callback) {
-  const observable = observablesByProxy.get(observableProxy);
-  return observable && typeof callback === 'function' ? observable.observe(callback) : false;
+export const unobserve = (observableProxy, callback) => {
+  const observers = observersByProxy.get(observableProxy);
+  return observers && typeof callback === 'function' ? observers.delete(callback) : false;
 }
 
-export function unobserve(observableProxy, callback) {
-  const observable = observablesByProxy.get(observableProxy);
-  return observable && typeof callback === 'function' ? observable.unobserve(callback) : false;
-}
-
-export function destroy(observableProxy) {
-  const observable = observablesByProxy.get(observableProxy);
-  return observable ? observable.destroy() : false;
+export const destroy = (observableProxy) => {
+  const observers = observersByProxy.get(observableProxy);
+  if(observers) {
+    observers.clear();
+    observables.delete(observablesByProxy.get(observableProxy));
+    observablesByProxy.delete(observableProxy);
+    observersByProxy.delete(observableProxy);
+    proxiesToNotify.delete(observableProxy);
+    return true;
+  }
+  return false;
 }
 
 tick();
