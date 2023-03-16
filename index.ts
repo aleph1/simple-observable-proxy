fc216c719159bc897aa601aa1e248f1e4a51a6d5
Daniel Barber <d.barber@aleph-1.com>
2023-03-16 07:43:32 -0400

    Rewrote index JavaScript as TypeScript

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+type PlainObject = { [name: string]: any }
+
 // data that is being observed
 const observables = new Set();
 // original data for each proxy
@@ -8,9 +10,9 @@ const observersByProxy = new Map();
 const proxiesToNotify = new Set();
 
 const objectConstructor = {}.constructor;
-const canBeObservable = data => !!data && (Array.isArray(data) || typeof data === 'object' && data.constructor === objectConstructor);
+const canBeObservable = (data: any): data is PlainObject => !!data && (Array.isArray(data) || typeof data === 'object' && data.constructor === objectConstructor);
 
-const tick = () => {
+const tick = ():void => {
   proxiesToNotify.forEach(proxy => {
     observersByProxy.get(proxy).forEach(callback => callback());
   });
@@ -18,13 +20,15 @@ const tick = () => {
   window.requestAnimationFrame(tick);
 };
 
-const makeObservableProxy = (data, rootProxy) => {
+const makeObservableProxy = (data: any, rootProxy?: any): PlainObject => {
   if(observables.has(data)) throw new Error('Can’t observe Object or Array again');
+  if(observablesByProxy.has(data)) throw new Error('rootProxy isn’t an observable');
+  let observers: Set<() => void>;
   const proxy = new Proxy(data, {
-    get(target, key) {
+    get(target, key): any {
       return target[key];
     },
-    set(target, key, value) {
+    set(target, key, value): boolean {
       if(target[key] !== value) {
         if(observers) {
           if(observablesByProxy.has(value)) throw new Error('Can’t nest observables');
@@ -34,7 +38,7 @@ const makeObservableProxy = (data, rootProxy) => {
       }
       return true;
     },
-    deleteProperty(target, key) {
+    deleteProperty(target, key): boolean {
       if (key in target) {
         delete target[key];
         proxiesToNotify.add(rootProxy || proxy);
@@ -48,28 +52,28 @@ const makeObservableProxy = (data, rootProxy) => {
     if(observablesByProxy.has(value)) throw new Error('Can’t nest observables');
     if(/*#__INLINE__*/canBeObservable(value)) proxy[key] = makeObservableProxy(value, rootProxy || proxy);
   });
-  const observers = new Set();
+  observers = new Set();
   observables.add(data);
   observablesByProxy.set(proxy, data);
   observersByProxy.set(proxy, observers);
-  return proxy;
+  return proxy as PlainObject;
 };
 
-export const observable = (data) => {
+export const observable = (data): PlainObject => {
   return /*#__INLINE__*/canBeObservable(data) && makeObservableProxy(data);
 }
 
-export const observe = (observableProxy, callback) => {
+export const observe = (observableProxy, callback: () => void): boolean => {
   const observers = observersByProxy.get(observableProxy);
   return observers && typeof callback === 'function' ? observers.add(callback) : false;
 }
 
-export const unobserve = (observableProxy, callback) => {
+export const unobserve = (observableProxy, callback: () => void): boolean => {
   const observers = observersByProxy.get(observableProxy);
   return observers && typeof callback === 'function' ? observers.delete(callback) : false;
 }
 
-export const destroy = (observableProxy) => {
+export const destroy = (observableProxy): boolean => {
   const observers = observersByProxy.get(observableProxy);
   if(observers) {
     observers.clear();
