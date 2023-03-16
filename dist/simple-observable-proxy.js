6e35bdaa28d10ae434eb74be5739dc4c6ac7777a
Daniel Barber <d.barber@aleph-1.com>
2023-03-16 07:44:22 -0400

    Built latest version

--- dist/simple-observable-proxy.js
+++ dist/simple-observable-proxy.js
@@ -1,1 +1,92 @@
-const e=new Set,t=new Map,r=new Map,o=new Set,n={}.constructor,s=()=>{o.forEach((e=>{r.get(e).forEach((e=>e()))})),o.clear(),window.requestAnimationFrame(s)},a=(s,c)=>{if(e.has(s))throw new Error("Can’t observe Object or Array again");const d=new Proxy(s,{get:(e,t)=>e[t],set(e,r,n){if(e[r]!==n){if(y){if(t.has(n))throw new Error("Can’t nest observables");o.add(c||d)}e[r]=n}return!0},deleteProperty:(e,t)=>t in e&&(delete e[t],o.add(c||d),!0)});(Array.isArray(s)?[...Array(s.length).keys()]:Object.keys(s)).forEach((e=>{const r=s[e];if(t.has(r))throw new Error("Can’t nest observables");(e=>!!e&&(Array.isArray(e)||"object"==typeof e&&e.constructor===n))(r)&&(d[e]=a(r,c||d))}));const y=new Set;return e.add(s),t.set(d,s),r.set(d,y),d};export const observable=e=>(e=>!!e&&(Array.isArray(e)||"object"==typeof e&&e.constructor===n))(e)&&a(e);export const observe=(e,t)=>{const o=r.get(e);return!(!o||"function"!=typeof t)&&o.add(t)};export const unobserve=(e,t)=>{const o=r.get(e);return!(!o||"function"!=typeof t)&&o.delete(t)};export const destroy=n=>{const s=r.get(n);return!!s&&(s.clear(),e.delete(t.get(n)),t.delete(n),r.delete(n),o.delete(n),!0)};s();
+/*! simple observable proxy v1.0.0 | MIT License | © 2022 Aleph1 Technologies Inc */
+const observables = new Set(),
+  observablesByProxy = new Map(),
+  observersByProxy = new Map(),
+  proxiesToNotify = new Set(),
+  objectConstructor = {}.constructor,
+  tick = () => {
+    proxiesToNotify.forEach((proxy) => {
+      observersByProxy.get(proxy).forEach((callback) => callback());
+    }),
+      proxiesToNotify.clear(),
+      window.requestAnimationFrame(tick);
+  },
+  makeObservableProxy = (data, rootProxy) => {
+    if (observables.has(data))
+      throw new Error("Can’t observe Object or Array again");
+    if (observablesByProxy.has(data))
+      throw new Error("rootProxy isn’t an observable");
+    let observers;
+    const proxy = new Proxy(data, {
+      get: (target, key) => target[key],
+      set(target, key, value) {
+        if (target[key] !== value) {
+          if (observers) {
+            if (observablesByProxy.has(value))
+              throw new Error("Can’t nest observables");
+            proxiesToNotify.add(rootProxy || proxy);
+          }
+          target[key] = value;
+        }
+        return !0;
+      },
+      deleteProperty: (target, key) =>
+        key in target &&
+        (delete target[key], proxiesToNotify.add(rootProxy || proxy), !0),
+    });
+    return (
+      (Array.isArray(data)
+        ? [...Array(data.length).keys()]
+        : Object.keys(data)
+      ).forEach((key) => {
+        const value = data[key];
+        if (observablesByProxy.has(value))
+          throw new Error("Can’t nest observables");
+        ((data) =>
+          !!data &&
+          (Array.isArray(data) ||
+            ("object" == typeof data &&
+              data.constructor === objectConstructor)))(value) &&
+          (proxy[key] = makeObservableProxy(value, rootProxy || proxy));
+      }),
+      (observers = new Set()),
+      observables.add(data),
+      observablesByProxy.set(proxy, data),
+      observersByProxy.set(proxy, observers),
+      proxy
+    );
+  },
+  observable = (data) =>
+    ((data) =>
+      !!data &&
+      (Array.isArray(data) ||
+        ("object" == typeof data && data.constructor === objectConstructor)))(
+      data
+    ) && makeObservableProxy(data),
+  observe = (observableProxy, callback) => {
+    const observers = observersByProxy.get(observableProxy);
+    return (
+      !(!observers || "function" != typeof callback) && observers.add(callback)
+    );
+  },
+  unobserve = (observableProxy, callback) => {
+    const observers = observersByProxy.get(observableProxy);
+    return (
+      !(!observers || "function" != typeof callback) &&
+      observers.delete(callback)
+    );
+  },
+  destroy = (observableProxy) => {
+    const observers = observersByProxy.get(observableProxy);
+    return (
+      !!observers &&
+      (observers.clear(),
+      observables.delete(observablesByProxy.get(observableProxy)),
+      observablesByProxy.delete(observableProxy),
+      observersByProxy.delete(observableProxy),
+      proxiesToNotify.delete(observableProxy),
+      !0)
+    );
+  };
+tick();
+export { destroy, observable, observe, unobserve };
