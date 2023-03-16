29c9a48ffd721b91efa4ccd0843ea06c4627672c
Daniel Barber <d.barber@aleph-1.com>
2023-03-16 09:47:34 -0400

    Converted jest setup to TypeScript

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,12 +5,12 @@ declare global {
 global.FRAME_TIME = 16;
 Object.defineProperty(window, 'navigator', {value: 'node'});
 Object.defineProperty(window, 'userAgent', {value: 'node'});
-Object.defineProperty(window, 'requestAnimationFrame', {value: callback =>{}});
+Object.defineProperty(window, 'requestAnimationFrame', {value: (callback:any) =>{}});
 
 jest.useFakeTimers();
 const spiedRequestAnimationFrame = jest.spyOn(window, 'requestAnimationFrame');
 spiedRequestAnimationFrame.mockImplementation((callback: FrameRequestCallback): number => {
-  return window.setTimeout(callback, FRAME_TIME);
+  return window.setTimeout(callback, global.FRAME_TIME);
 });
 
 afterAll(() => {
