75cbf53792749ac35087fde2965683054479b739
Daniel Barber <d.barber@aleph-1.com>
2023-03-16 16:02:42 -0400

    export FRAME_TIME to avoid jest test errors

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,7 +2,8 @@ declare global {
   var FRAME_TIME: number;
 }
 
-global.FRAME_TIME = 16;
+export const FRAME_TIME: number = global.FRAME_TIME = 16;
+
 Object.defineProperty(window, 'navigator', {value: 'node'});
 Object.defineProperty(window, 'userAgent', {value: 'node'});
 Object.defineProperty(window, 'requestAnimationFrame', {value: (callback:any) =>{}});
