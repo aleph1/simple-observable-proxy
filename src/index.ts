c08d42df2b1fa68a79819d8b525982ed39d917ce
Daniel Barber <d.barber@aleph-1.com>
2023-12-27 16:06:03 -0500

    Disabled unused code

--- src/index.ts
+++ src/index.ts
@@ -49,8 +49,8 @@ const tick = (): void => {
 };
 
 const makeObservableProxy = (data: Observable, rootProxy?: Observable): Observable => {
-  if(rootProxy && !observablesByProxy.has(rootProxy)) throw new Error('rootProxy isn’t an observable');
   if(observables.has(data) || observablesByProxy.has(data)) throw new Error('data is alreaby an observable');
+  //if(rootProxy && !observablesByProxy.has(rootProxy)) throw new Error('rootProxy isn’t an observable');
   let observers: ObservableCallbackObject;
   const proxy = new Proxy(data, {
     get(target, key): any {
