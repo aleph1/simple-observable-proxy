2fe52d5c6a3459cd0338d276b233a305d77bea16
Daniel Barber <d.barber@aleph-1.com>
2024-01-08 15:48:33 -0500

    Built latest version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -13,15 +13,17 @@ export declare const observableEvents: {
     readonly change: "change";
     readonly destroy: "destroy";
 };
-type observableEventKey = keyof typeof observableEvents;
-export type observableEvent = typeof observableEvents[observableEventKey];
-export declare const isPlainObject: (data: Observable) => boolean;
+type ObservableEventKey = keyof typeof observableEvents;
+export type ObservableEvent = typeof observableEvents[ObservableEventKey];
+export declare const isPlainObject: (data: {
+    [name: string]: any;
+}) => boolean;
 export declare const observable: (data: Observable) => Observable;
 export declare const isObservable: (observableProxy: Observable) => boolean;
 export declare const observableObject: (data: ObservableObject) => ObservableObject;
 export declare const observableArray: (data: ObservableObject) => ObservableArray;
 export declare const observableMap: (data: ObservableMap) => ObservableMap;
-export declare const on: (observableProxy: Observable, eventType: observableEventKey, callback: ObservableCallback) => boolean;
-export declare const off: (observableProxy: Observable, eventType: observableEventKey, callback: ObservableCallback) => boolean;
+export declare const on: (observableProxy: Observable, eventType: ObservableEventKey, callback: ObservableCallback) => boolean;
+export declare const off: (observableProxy: Observable, eventType: ObservableEventKey, callback: ObservableCallback) => boolean;
 export declare const destroy: (observableProxy: Observable) => boolean;
 export {};
