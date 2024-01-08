45994b44f8832bcfefa314bbf333ad46cdee5022
Daniel Barber <d.barber@aleph-1.com>
2024-01-08 01:41:45 -0500

    Built latest version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,13 @@
-export type Observable = {
+export type ObservableArray = {
     [name: string]: any;
 };
+export type ObservableObject = {
+    [name: string]: any;
+};
+export type ObservableMap = {
+    [name: string]: any;
+};
+export type Observable = ObservableArray | ObservableObject | ObservableMap;
 export type ObservableCallback = (proxy: Observable) => void;
 export declare const ObservableEvents: {
     readonly change: "change";
@@ -14,9 +21,10 @@ type ObservableCallbackObject = {
 };
 export type ObservableCallbackMap = Map<Observable, ObservableCallbackObject>;
 export declare const observable: (data: Observable) => Observable;
+export declare const observableObject: (data: ObservableObject) => ObservableObject;
+export declare const observableArray: (data: ObservableObject) => ObservableArray;
+export declare const observableMap: (data: ObservableMap) => ObservableMap;
 export declare const on: (observableProxy: Observable, eventType: ObservableEventKey, callback: ObservableCallback) => boolean;
 export declare const off: (observableProxy: Observable, eventType: ObservableEventKey, callback: ObservableCallback) => boolean;
-export declare const observe: (observableProxy: Observable, callback: ObservableCallback) => boolean;
-export declare const unobserve: (observableProxy: Observable, callback: ObservableCallback) => boolean;
 export declare const destroy: (observableProxy: Observable) => boolean;
 export {};
