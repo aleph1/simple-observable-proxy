3b8ac8c61326ff55eca9800179f727d0bf920b7b
Daniel Barber <d.barber@aleph-1.com>
2024-01-08 15:35:26 -0500

    Built latest version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,24 +9,19 @@ export type ObservableMap = {
 };
 export type Observable = ObservableArray | ObservableObject | ObservableMap;
 export type ObservableCallback = (proxy: Observable) => void;
-export declare const ObservableEvents: {
+export declare const observableEvents: {
     readonly change: "change";
     readonly destroy: "destroy";
 };
-type ObservableEventKey = keyof typeof ObservableEvents;
-export type ObservableEvent = typeof ObservableEvents[ObservableEventKey];
-type ObservableCallbackObject = {
-    change: Set<ObservableCallback>;
-    destroy: Set<ObservableCallback>;
-};
-export type ObservableCallbackMap = Map<Observable, ObservableCallbackObject>;
+type observableEventKey = keyof typeof observableEvents;
+export type observableEvent = typeof observableEvents[observableEventKey];
 export declare const isPlainObject: (data: Observable) => boolean;
 export declare const observable: (data: Observable) => Observable;
 export declare const isObservable: (observableProxy: Observable) => boolean;
 export declare const observableObject: (data: ObservableObject) => ObservableObject;
 export declare const observableArray: (data: ObservableObject) => ObservableArray;
 export declare const observableMap: (data: ObservableMap) => ObservableMap;
-export declare const on: (observableProxy: Observable, eventType: ObservableEventKey, callback: ObservableCallback) => boolean;
-export declare const off: (observableProxy: Observable, eventType: ObservableEventKey, callback: ObservableCallback) => boolean;
+export declare const on: (observableProxy: Observable, eventType: observableEventKey, callback: ObservableCallback) => boolean;
+export declare const off: (observableProxy: Observable, eventType: observableEventKey, callback: ObservableCallback) => boolean;
 export declare const destroy: (observableProxy: Observable) => boolean;
 export {};
